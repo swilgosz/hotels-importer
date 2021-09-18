5d8e16b6876bc5ec26991a32ae0dc0df8cf84af9
Sebastian Wilgosz <sebastian@driggl.com>
2021-09-19 01:02:55 +0200

    Add index action

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::HotelsController < ApplicationController
+  def index
+    render json: {}
+  end
 end
--- spec/requests/api/v1/hotels_spec.rb
+++ spec/requests/api/v1/hotels_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Hotels", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'has :ok status code' do
+      get '/api/v1/hotels'
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
